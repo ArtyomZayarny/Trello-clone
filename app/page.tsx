60c314b98a98d0f901763be0c0becb1d2c318596
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-29 14:35:01 +0300

    instal dnd lib

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Header />
-      <h1>Trello 2.0</h1>
+      <Board />
     </main>
   );
 }
