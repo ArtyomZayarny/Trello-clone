1e6416ec168da92f826bff68d3c13afb0502bf03
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-29 15:39:28 +0300

    install state manager zustand

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 export default function Board() {
+  useEffect(() => {}, []);
   return (
     <DragDropContext>
       <Droppable droppableId="board" direction="horizontal">
