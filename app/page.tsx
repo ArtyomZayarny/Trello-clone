22611af2629a3bfa5091113eaae62a6fed9490e0
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-29 11:04:58 +0300

    Added header

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <h1>Trello 2.0</h1>
     </main>
   );
