2071e8c88ccd1bba7c9946c906f1190bf9accbd0
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-29 09:35:36 +0300

    init

---
+++ trello-clone/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,14 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/artemzaiarnyi/Downloads/Learn/NextJs/Trello-clone/trello-clone/app/layout.tsx": [
+      "/Users/artemzaiarnyi/Downloads/Learn/NextJs/Trello-clone/trello-clone/app/globals.css",
+      "/Users/artemzaiarnyi/Downloads/Learn/NextJs/Trello-clone/trello-clone/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "cssModules": {
+    "/Users/artemzaiarnyi/Downloads/Learn/NextJs/Trello-clone/trello-clone/app/page": [
+      "/Users/artemzaiarnyi/Downloads/Learn/NextJs/Trello-clone/trello-clone/app/globals.css",
+      "/Users/artemzaiarnyi/Downloads/Learn/NextJs/Trello-clone/trello-clone/node_modules/next/font/google/target.css?{\"path\":\"app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
