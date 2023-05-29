6ad5f4013aac9810d8576e3e89a993193b46c425
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-29 14:17:29 +0300

    added GRT section

--- components/Header.tsx
+++ components/Header.tsx
@@ -36,6 +36,12 @@ export default function Header() {
           <Avatar name="Artem Zaiarnyi" round color="#0055D1" size="50" />
         </div>
       </div>
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          GPT is sumarising your tasks for day...
+        </p>
+      </div>
     </header>
   );
 }
