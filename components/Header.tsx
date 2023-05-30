4f92ec5afcfc4b7c0c49751725163dc55ed54c4c
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-30 13:45:04 +0300

    added input handle

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,13 @@ import Image from "next/image";
 import React from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 export default function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl ">
@@ -43,6 +48,8 @@ export default function Header() {
               type="text"
               placeholder="Search"
               className="flex-1 outline-none p-2"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
             />
             <button type="submit" hidden>
               Search
