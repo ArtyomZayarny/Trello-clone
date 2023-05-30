93b5ee4d26e6f2bd591a6a0ec22d83ec6ab13022
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-30 15:28:29 +0300

    Added GPR connections

---
+++ app/api/generateSummary/route.ts
@@ -0,0 +1,32 @@
+import openai from "@/openai";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  //todos in the body of th POST req
+  const { todos } = await request.json();
+
+  //comminicate with GPT
+  const response = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo", // 'gpt-4'
+    temperature: 0.8,
+    n: 1,
+    stream: false,
+    messages: [
+      {
+        role: "system",
+        content: `When responding, welcome the user always as Artem Zaiarnyi and say welcome to the Trello clone app!
+        Limit the response to 200 characters`,
+      },
+      {
+        role: "user",
+        content: `Hi therre, provide a summary of the following todos. Count how many todos are in each category 
+        such as To do, In progress and done, then tell the user to have a productive day! Here's the data ${JSON.stringify(
+          todos
+        )}`,
+      },
+    ],
+  });
+
+  const { data } = response;
+  return NextResponse.json(data.choices[0].message);
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,35 @@
 "use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
+import { fetchSuggestion } from "../lib/fetchSuggestion";
 
 export default function Header() {
-  const [searchString, setSearchString] = useBoardStore((state) => [
+  const [board, searchString, setSearchString] = useBoardStore((state) => [
+    state.board,
     state.searchString,
     state.setSearchString,
   ]);
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [suggestion, setSuggestion] = useState<string>("");
+
+  useEffect(() => {
+    if (board.columns.size == 0) return;
+    setLoading(true);
+
+    const fetchSuggestionFunc = async () => {
+      const suggestion = await fetchSuggestion(board);
+      setSuggestion(suggestion);
+      setLoading(false);
+    };
+
+    fetchSuggestionFunc();
+  }, [board]);
+
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl ">
@@ -60,8 +79,14 @@ export default function Header() {
       </div>
       <div className="flex items-center justify-center px-5 py-5 md:py-5">
         <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
-          GPT is sumarising your tasks for day...
+          <UserCircleIcon
+            className={`inline-block h-10 w-10 text-[#0055D1] mr-1 ${
+              loading && "animate-spin"
+            }`}
+          />
+          {suggestion && !loading
+            ? suggestion
+            : "GPT is sumarising your tasks for day..."}
         </p>
       </div>
     </header>
