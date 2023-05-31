6e0682128f698866f0ac84cf7605258c59333e2f
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-05-31 12:38:01 +0300

    fix issues

--- lib/getUrl.tsx
+++ lib/getUrl.tsx
@@ -1,6 +1,6 @@
 import { storage } from "@/appwrite";
 
-export const getUrl = async (image: InstanceType<typeof Image>) => {
+export const getUrl = async (image: Image) => {
   const url = storage.getFilePreview(image.bucketId, image.fileId);
 
   return url;
